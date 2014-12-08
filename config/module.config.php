e55387133b5dbc2154f90a0f3d37e542bfdb76df
Anders <anders@jaypan.com>
2014-12-08 13:29:57 +0900

    Init

---
+++ config/module.config.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace KryuuSimpleMessage;
+
+/**
+ * @encoding UTF-8
+ * @note *
+ * @todo *
+ * @package PackageName
+ * @author Anders Blenstrup-Pedersen - KatsuoRyuu <anders-github@drake-development.org>
+ * @license *
+ * The Ryuu Technology License
+ *
+ * Copyright 2014 Ryuu Technology by 
+ * KatsuoRyuu <anders-github@drake-development.org>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * Ryuu Technology shall be visible and readable to anyone using the software 
+ * and shall be written in one of the following ways: Ryuu Technology 
+ * or by using the company logo.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @version 20140730 
+ * @link https://github.com/KatsuoRyuu/
+ */
+
+return array(
+    __NAMESPACE__ => array(
+    ),
+    'view_manager' => array(
+        'template_map' => array(
+            'partial/messages/view'				=> __DIR__ . '/../view/kryuu-simple-message/partial/messages/view.phtml',
+			
+            'partial/messages/error/head'		=> __DIR__ . '/../view/kryuu-simple-message/partial/messages/error/head.phtml',
+            'partial/messages/error/message'	=> __DIR__ . '/../view/kryuu-simple-message/partial/messages/error/message.phtml',
+            'partial/messages/error/foot'		=> __DIR__ . '/../view/kryuu-simple-message/partial/messages/error/foot.phtml',
+			            
+			'partial/messages/warning/head'		=> __DIR__ . '/../view/kryuu-simple-message/partial/messages/warning/head.phtml',
+            'partial/messages/warning/message'	=> __DIR__ . '/../view/kryuu-simple-message/partial/messages/warning/message.phtml',
+            'partial/messages/warning/foot'		=> __DIR__ . '/../view/kryuu-simple-message/partial/messages/warning/foot.phtml',
+			            
+			'partial/messages/success/head'		=> __DIR__ . '/../view/kryuu-simple-message/partial/messages/success/head.phtml',
+            'partial/messages/success/message'	=> __DIR__ . '/../view/kryuu-simple-message/partial/messages/success/message.phtml',
+            'partial/messages/success/foot'		=> __DIR__ . '/../view/kryuu-simple-message/partial/messages/success/foot.phtml',
+			            
+			'partial/messages/info/head'		=> __DIR__ . '/../view/kryuu-simple-message/partial/messages/info/head.phtml',
+            'partial/messages/info/message'		=> __DIR__ . '/../view/kryuu-simple-message/partial/messages/info/message.phtml',
+            'partial/messages/info/foot'		=> __DIR__ . '/../view/kryuu-simple-message/partial/messages/info/foot.phtml',
+			            
+			'partial/messages/default/head'		=> __DIR__ . '/../view/kryuu-simple-message/partial/messages/default/head.phtml',
+            'partial/messages/default/message'	=> __DIR__ . '/../view/kryuu-simple-message/partial/messages/default/message.phtml',
+            'partial/messages/default/foot'		=> __DIR__ . '/../view/kryuu-simple-message/partial/messages/default/foot.phtml',
+        ),
+    ),
+    'view_helpers' => array(
+        'invokables' => array(
+            'messenger'				=> 'KryuuSimpleMessage\View\Helper\FlashMessengerHelper',
+            //'viewAllBlogPosts'  => 'KryuuSimpleMessage\View\Helper\ViewAllPostsHelper',
+        ),
+    ),
+);
---
+++ src/KryuuFlashMessengerTest/View/Helper/FlashMessengerHelper.php
@@ -0,0 +1,75 @@
+<?php
+namespace KryuuSimpleMessage\View\Helper;
+
+use Zend\View\Helper\AbstractHelper;
+use Zend\ServiceManager\ServiceLocatorAwareInterface; 
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class FlashMessengerHelper extends AbstractHelper implements ServiceLocatorAwareInterface
+{    
+    
+    public function __invoke($output=TRUE)
+    {
+		$this->setMessenger(
+			$this->getServiceLocator()->get('flash_messenger')
+		);
+		if ($output == TRUE)
+		{
+			return $this->getView()->render('partial/messages/view', array('flashMessenger' => $this->getMessenger()));
+		}
+		return $this;
+    }
+
+	public function getServiceLocator()
+	{
+		return $this->serviceLocator;
+	}
+
+	public function setServiceLocator(ServiceLocatorInterface $serviceLocator)
+	{
+		$this->serviceLocator = $serviceLocator;
+	}
+	
+	public function getError($data=array())
+	{
+		return $this->getView()->render('partial/messages/error/head', array('data' =>$data)).
+			$this->getView()->render('partial/messages/error/message', array('messenger' => $this->getMessenger())).
+			$this->getView()->render('partial/messages/error/foot', array('data' =>$data));
+	}
+	public function getWarning($data=array())
+	{
+		return $this->getView()->render('partial/messages/warning/head', array('data' =>$data)).
+			$this->getView()->render('partial/messages/warning/message', array('messenger' => $this->getMessenger())).
+			$this->getView()->render('partial/messages/warning/foot', array('data' =>$data));
+	}
+	public function getSuccess($data=array())
+	{
+		return $this->getView()->render('partial/messages/success/head', array('data' =>$data)).
+			$this->getView()->render('partial/messages/success/message', array('messenger' => $this->getMessenger())).
+			$this->getView()->render('partial/messages/success/foot', array('data' =>$data));
+	}
+	public function getInfo($data=array())
+	{
+		return $this->getView()->render('partial/messages/info/head', array('data' =>$data)).
+			$this->getView()->render('partial/messages/info/message', array('messenger' => $this->getMessenger())).
+			$this->getView()->render('partial/messages/info/foot', array('data' =>$data));
+	}
+	public function getDefault($data=array())
+	{
+		return $this->getView()->render('partial/messages/default/head', array('data' =>$data)).
+			$this->getView()->render('partial/messages/default/message', array('messenger' => $this->getMessenger())).
+			$this->getView()->render('partial/messages/default/foot', array('data' =>$data));
+	}
+	
+	private function setMessenger($messenger)
+	{
+		$this->messenger = $messenger;
+	}
+	
+	private function getMessenger()
+	{
+		return $this->messenger;
+	}
+	
+
+}
